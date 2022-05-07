cdade361e4a0702a0db0554f772fb466fafb38c4
sabgilhun <sabgilhun@gmail.com>
2022-05-07 23:55:33 +0900

    LoginFragment 리팩토링

--- android/app/src/main/java/com/example/sidedish/ui/login/LoginFragment.kt
+++ android/app/src/main/java/com/example/sidedish/ui/login/LoginFragment.kt
@@ -1,52 +1,48 @@
 package com.example.sidedish.ui.login
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.fragment.app.Fragment
 import com.example.sidedish.R
 import com.example.sidedish.databinding.FragmentLoginBinding
-import com.example.sidedish.ui.viewmodel.MenuListViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.HttpUrl
 
 class LoginFragment : Fragment() {
 
-    private val binding: FragmentLoginBinding by lazy {
-        FragmentLoginBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.signInButton.setOnClickListener {
-            login()
-        }
+        binding.signInButton.setOnClickListener { login() }
     }
 
     private fun login() {
-        val httpUrl = HttpUrl.Builder()
+        val context = this.context ?: return
+
+        val uri = Uri.Builder()
             .scheme("http")
-            .host("github.com")
-            .addPathSegment("login")
-            .addPathSegment("oauth")
-            .addPathSegment("authorize")
-            .addQueryParameter("client_id", getString(R.string.github_client_id))
-            .addQueryParameter("redirect_uri", getString(R.string.github_redirect_url))
-            .addQueryParameter("scope", "user")
+            .authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", getString(R.string.github_client_id))
+            .appendQueryParameter("redirect_uri", getString(R.string.github_redirect_url))
             .build()
 
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(httpUrl.toString()))
-        startActivity(intent)
+        CustomTabsIntent.Builder()
+            .build()
+            .launchUrl(context, uri)
     }
 }
